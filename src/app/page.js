8b69aade684d0fa8fa4b80c19f5f2512b1a98f04
coder_xujie <coder_xujie@163.com>
2024-04-09 22:42:12 +0800

    1

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,51 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import page from "./page.module.css";
+// 以 use 开头的函数叫做 hook
+import {useState} from 'react';
 
+// React 组件是返回标签的 JS 函数
+// 组件名必须是以大写字母开头
+// JSX 语法，比 HTML 更加严格，标签必须闭合，并且一个组件不能返回多个 JSX 标签
+function MyButton() {
+  // count 使用的值
+  // setCount 用于更新值的函数
+  let [count, setCount] = useState(0)
+  function handleClick() {
+    alert(`click me: ${count}`)
+    setCount(count++)
+  }
+  return <button onClick={handleClick}>我是一个按钮</button>;
+}
+
+// default 关键字指定了当前文件的主要组件
 export default function Home() {
+  const card = "card-btn";
+  const userName = "hello React";
+  // 可以直接使用变量定义组件
+  const btnCpt = <MyButton></MyButton>;
+  const products = [
+    { title: "Cabbage", isFruit: false, id: 1 },
+    { title: "Garlic", isFruit: false, id: 2 },
+    { title: "Apple", isFruit: true, id: 3 },
+  ];
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+      {/* 通过 CSS module 的形式来使用 CSS */}
+      <div className={page[card]}>
+        {/* 通过 {} 在 JSX 中使用 JS 变量 */}
+        <h1>{userName}</h1>
+        {btnCpt}
+        <ul>
+          {products.map((product) => (
+            <li
+              key={product.id}
+              style={{ color: product.isFruit ? "magenta" : "darkgreen" }}
+            >
+              {product.title}
+            </li>
+          ))}
+        </ul>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
